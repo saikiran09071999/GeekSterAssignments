c9daf89a0a7992cc88af4a2088580b4283b03616
saikiran09071999 <94847377+saikiran09071999@users.noreply.github.com>
2021-12-15 14:30:14 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,9 @@
+class Pattern2{
+  public static void main(String args[]){
+  for(int i=0;i<4;i++){
+    for(int j=0;j<=i;j++)
+       System.out.print(j+1);
+    System.out.println("");
+  } 
+ }
+}
