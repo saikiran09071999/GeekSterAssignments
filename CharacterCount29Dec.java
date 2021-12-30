69d6ebfe591c00b14b375d225b2f34c55edee571
saikiran09071999 <94847377+saikiran09071999@users.noreply.github.com>
2021-12-30 15:18:33 +0530

    29-12-2021 assignment

---
+++ CharacterCount29Dec.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class CharacterCount29Dec{
+  public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter string");
+   String str1=sc.nextLine();
+   System.out.println(myLength(str1)); 
+ }
+ public static int myLength(String str1){
+    return str1.length();
+ }
+}
---
+++ StringConcat29Dec.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class StringConcat29Dec{
+  public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter two strings");
+   String str1=sc.nextLine();
+   String str2=sc.nextLine();
+   System.out.println(myConcat(str1,str2)); 
+ }
+ public static String myConcat(String str1,String str2){
+    return str1+" "+str2;
+ }
+}
